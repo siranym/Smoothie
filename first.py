76b3305985b982577ba2f2150fa08145d45a75a0
Naresh Kumar Siranyam <naresh.siranyam@gmail.com>
2025-06-05 19:54:41 -0400

    Update first.py

--- first.py
+++ first.py
@@ -5,25 +5,15 @@ from snowflake.snowpark.session import Session
 def create_session_object():
     # Define the connection parameters
     connection_parameters = {
-account = "QECGRSE-HJC54946"
-user = "SIRANYAM"
-password = "Nare2819#npdv1"
-role = "SYSADMIN"
-warehouse = "COMPUTE_WH"
-database = "SMOOTHIES"
-schema = "PUBLIC"
-client_session_keep_alive = true 
-
-
-"account": "__INSERT_ACCOUNT_HERE__",
-      "user": "__INSERT_USERNAME_HERE__",
-      "password": "__INSERT_PASSWORD_HERE__",
-      "role": "__INSERT_ROLE_HERE__",
-      "warehouse": "__INSERT_WAREHOUSE_HERE__",
-      "database": "__INSERT_DATABASE_HERE__",
-      "schema": "__INSERT_SCHEMA_HERE__"
+    account = "QECGRSE-HJC54946"
+    user = "SIRANYAM"
+    password = "Nare2819#npdv1"
+    role = "SYSADMIN"    
+    warehouse = "COMPUTE_WH"
+    database = "SMOOTHIES"
+    schema = "PUBLIC"
+    client_session_keep_alive = true 
     }
-    
     
     # Create the session object with the provided connection parameters
     session = Session.builder.configs(connection_parameters).create()
